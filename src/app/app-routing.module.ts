a7aee414ec6ab98afed9d8695e72db947b4eee0e
masha <marmylad@gmail.com>
2021-12-06 16:39:09 +0200

    добавлена навигация

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,69 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {ExtraOptions, RouterModule, Routes} from '@angular/router';
+import {MainMenuComponent} from "./main/main-menu.component";
+import {HomePageComponent} from "./home-page/home-page.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent
+    // redirectTo: 'catalog',
+    // pathMatch:'full'
+  },
+
+  // {
+  //   path: 'register',
+  //   component: RegisterComponent
+  // },
+  // {
+  //   path: 'order-register',
+  //   component: OrderRegisterComponent
+  // },
+  // {
+  //   path: 'products',
+  //   children: [
+  //     {
+  //       path: '',
+  //       loadChildren: () =>
+  //         import('./catalog/catalog.module')
+  //           .then(mod => mod.CatalogModule)
+  //     },
+  //
+  //     {
+  //       path:':id',
+  //       canLoad: [ProductGuard],
+  //       loadChildren: () =>
+  //         import('./product-page/product-page.module')
+  //           .then(mod => mod.ProductPageModule)
+  //     }
+  //
+  //
+  //   ]
+  // },
+
+  //
+  // {
+  //   path:'error',
+  //   component: ErrorPageComponent
+  // },
+
+  {
+    path: '**',
+    redirectTo: 'error'
+  }
+
+]
+
+export const options: ExtraOptions = {
+  enableTracing: false,
+  scrollPositionRestoration: "enabled",
+  // preloadingStrategy: PreloadAllModules
+}
+
+
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from "@angular/material/button";
+
+import { MainMenuComponent } from './main/main-menu.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import {MatCardModule} from "@angular/material/card";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
